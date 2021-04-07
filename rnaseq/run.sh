b5e2dd8da32d9212fc6b15f7e4474dbc38081e3a
Maria Carilli <maca9088@colorado.edu>
2021-04-07 14:04:59 -0600

    file

--- rnaseq/run.sh
+++ rnaseq/run.sh
@@ -2,7 +2,7 @@
 #SBATCH -p long
 #SBATCH --job-name=HEPG2_rna_seq
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=john.rinn@colorado.edu
+#SBATCH --mail-user=maria.carilli@colorado.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=6gb
@@ -19,11 +19,11 @@ nextflow run nf-core/rnaseq -r 1.4.2 \
 -resume \
 -profile singularity \
 --reads 'fastq/*{_read1,_read2}.fastq.gz' \
---fasta /scratch/Shares/rinn/genomes/Homo_sapiens/Gencode/v32/GRCh38.p13.genome.fa \
---gtf /scratch/Shares/rinn/genomes/Homo_sapiens/Gencode/v32/gencode.v32.annotation.gtf \
+--fasta /scratch/Shares/rinnclass/data/genomes/GRCh38.p13.genome.fa \
+--gtf /scratch/Shares/rinnclass/data/genomes/gencode.v32.annotation.gtf \
 --pseudo_aligner salmon \
 --gencode \
---email john.rinn@colorado.edu \
+--email maria.carilli@colorado.edu \
 -c nextflow.config
 
 date
