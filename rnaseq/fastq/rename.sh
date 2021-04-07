f07940e8e7feb2f96913d633f37327d8da2eb9d6
Olivia <olivia.luyties@colorado.edu>
2021-04-07 14:13:07 -0600

    commit

---
+++ rnaseq/fastq/rename.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mv ENCFF876UIT.fastq.gz ENCSR887ZSY_rep2_read1.fastq.gz
+mv ENCFF423KUF.fastq.gz ENCSR887ZSY_rep1_read2.fastq.gz
+mv ENCFF902PAE.fastq.gz ENCSR887ZSY_rep1_read1.fastq.gz
+mv ENCFF567YOK.fastq.gz ENCSR887ZSY_rep2_read2.fastq.gz
+mv ENCFF339UJI.fastq.gz ENCSR813BDU_rep1_read2.fastq.gz
+mv ENCFF232QKC.fastq.gz ENCSR813BDU_rep2_read1.fastq.gz
+mv ENCFF859AWY.fastq.gz ENCSR813BDU_rep2_read2.fastq.gz
+mv ENCFF765REV.fastq.gz ENCSR813BDU_rep1_read1.fastq.gz
+mv ENCFF713MNU.fastq.gz ENCSR181ZGR_rep2_read1.fastq.gz
+mv ENCFF478DZZ.fastq.gz ENCSR181ZGR_rep2_read2.fastq.gz
+mv ENCFF936SLY.fastq.gz ENCSR181ZGR_rep1_read1.fastq.gz
+mv ENCFF446UEC.fastq.gz ENCSR181ZGR_rep1_read2.fastq.gz
+mv ENCFF888RBV.fastq.gz ENCSR862HPO_rep2_read2.fastq.gz
+mv ENCFF196RCZ.fastq.gz ENCSR862HPO_rep1_read1.fastq.gz
+mv ENCFF268FHH.fastq.gz ENCSR862HPO_rep2_read1.fastq.gz
+mv ENCFF740UPF.fastq.gz ENCSR862HPO_rep1_read2.fastq.gz
+mv ENCFF802YBN.fastq.gz ENCSR061SFU_rep2_read1.fastq.gz
+mv ENCFF720EGG.fastq.gz ENCSR061SFU_rep2_read2.fastq.gz
+mv ENCFF646ZDN.fastq.gz ENCSR061SFU_rep1_read2.fastq.gz
+mv ENCFF417ZEN.fastq.gz ENCSR061SFU_rep1_read1.fastq.gz
