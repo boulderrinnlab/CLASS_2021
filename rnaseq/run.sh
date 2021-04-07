32b3e3d5dc9807ade56759e1f27f83fcdf812554
James Pratt <japr9820@fiji-viz.colorado.edu>
2021-04-07 13:16:08 -0600

    RNAseq run file

--- rnaseq/run.sh
+++ rnaseq/run.sh
@@ -2,7 +2,7 @@
 #SBATCH -p long
 #SBATCH --job-name=HEPG2_rna_seq
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=john.rinn@colorado.edu
+#SBATCH --mail-user=james.pratt@colorado.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=6gb
@@ -20,10 +20,10 @@ nextflow run nf-core/rnaseq -r 3.0 \
 -profile singularity \
 --input design.csv \
 --aligner star_salmon \
---fasta /Shares/rinn_class/data/genomes/human/gencode/v32/GRCh38.p13.genome.fa \
---gtf /Shares/rinn_class/data/genomes/human/gencode/v32/gencode.v32.annotation.gtf \
+--fasta /scratch/Shares/rinnclass/data/genomes/GRCh38.p13.genome.fa \
+--gtf /scratch/Shares/rinnclass/data/genomes/gencode.v32.annotation.gtf \
 --gencode \
---email john.rinn@colorado.edu \
+--email james.pratt@colorado.edu \
 -c nextflow.config
 
 date
