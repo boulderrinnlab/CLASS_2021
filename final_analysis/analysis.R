f1317ba186895f2869f6b6e1770ee735bbe873a0
ChelseaT <chto3678@colorado.edu>
2021-04-19 14:24:09 -0600

    Added 02 and 03 .Rmd to set up for .md preparation

--- final_analysis/analysis.R
+++ final_analysis/analysis.R
@@ -8,3 +8,11 @@ rmarkdown::render(file.path("00_consensus_peaks",
 rmarkdown::render(file.path("01_global_peak_properties",
                             "01_global_peak_properties.Rmd"), 
                   md_document(variant = "markdown_github"))
+
+rmarkdown::render(file.path("02_exploring_bumps",
+                            "02_exploring_bumps.Rmd"), 
+                  md_document(variant = "markdown_github"))
+
+rmarkdown::render(file.path("03_promoter_heat_maps",
+                            "01_promoter_heat_maps.Rmd"), 
+                  md_document(variant = "markdown_github"))
