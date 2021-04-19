1d9f7d280929d00d9deb9fb5b404d7b48c4bc675
Olivia <olivia.luyties@colorado.edu>
2021-04-19 15:56:15 -0600

    01 knit

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,1 +1,5 @@
 # This file will eventually knit all the RMarkdowns
+library(rmarkdown)
+rmarkdown::render(file.path("00_consensus_peaks",
+                            "00_consensus_peaks.Rmd"), 
+                  md_document(variant = "markdown_github"))
