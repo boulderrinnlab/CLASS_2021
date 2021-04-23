b87c5f69a64ac80d820006fb25d32d77c4d1495b
Michael Smallegan <michael.smallegan@colorado.edu>
2021-04-23 00:50:23 -0600

    Oops, fix.

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,1 +1,5 @@
-# This file will eventually knit all the RMarkdowns
+library(rmarkdown)
+
+rmarkdown::render("09_high_binding_properties/high_binding_properties.Rmd",
+                  md_document(variant = "markdown_github"))
+                  
