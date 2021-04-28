1ec247c2b8d24d9e915b749ddb0e5821d2abd06a
Michael Smallegan <michael.smallegan@colorado.edu>
2021-04-28 13:25:14 -0600

    Updating some uncommitted things.

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -2,4 +2,14 @@ library(rmarkdown)
 
 rmarkdown::render("09_high_binding_properties/high_binding_properties.Rmd",
                   md_document(variant = "markdown_github"))
+
+rmarkdown::render("06_promoter_conservation/promoter_conservation.Rmd",
+                  md_document(variant = "markdown_github"))
+
+rmarkdown::render("10_repetitive_element_overlaps/repetitive_element_overlaps.Rmd",
+                  md_document(variant = "markdown_github"))
+
+
+rmarkdown::render("07_conservative_reservoirs/conservative_reservoirs.Rmd",
+                  md_document(variant = "markdown_github"))
                   
